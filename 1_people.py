8d5d90d294f1dfffcf8b7bb698e9cdac4bc674ef
SuperSocrates <treganblue@gmail.com>
2016-03-12 16:29:03 -0600

    space displaying names + craft

--- 1_people.py
+++ 1_people.py
@@ -3,18 +3,25 @@
 class Person:
 
   def __init__(self, name, hometown):
-    pass
+    self.name = name
+    self.hometown = hometown
 
   def introduce(self):
-    pass
+    print("Hi, I'm {0}. I'm from {1}.".format(self.name, self.hometown))
 
   def meet(self, another):
     self.introduce()
     another.introduce()
 
+class Employee(Person):
 
-me = Person("<your name>", "<your hometown>")
-friend = Person("Jeff", "Skokie")
+    def department(self):
+        return "Software Consulting"
+
+
+me = Employee("Tom", "Evanston")
+print(me.department())
+friend = Employee("Jeff", "Skokie")
 
 me.meet(friend)
 # Hi, I'm <your name>.  I'm from <your hometown>.
